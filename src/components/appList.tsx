0bae41f0af0c62ec7c89fe93ae2de507517be207
Bastian Meissner <bastian.meissner@st.oth-regensburg.de>
2020-07-08 19:36:36 +0200

    Test

---
+++ src/components/appList.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { AppCategory } from "./appCategory";
+
+import {
+  handleResponse,
+  Headline,
+  ListContainer,
+  ErrorMessage
+} from "./elements";
+
+const useAppData = () => {
+  const [appData, setAppData] = useState({
+    categories: [],
+    apps: [],
+    error: false
+  });
+  const fetchAppData = useCallback(() => {
+    (process.env.NODE_ENV === "production"
+      ? fetch("/data/apps.json").then(handleResponse)
+      : import("./data/apps.json")
+    )
+      .then(jsonResponse => {
+        setAppData({ ...jsonResponse, error: false });
+      })
+      .catch(error => {
+        setAppData({ categories: [], apps: [], error: error.message });
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchAppData();
+  }, [fetchAppData]);
+  return { appData, fetchAppData };
+};
+
+const AppList = () => {
+  const {
+    appData: { categories, apps, error }
+  } = useAppData();
+  return (
+    <ListContainer>
+      <Headline>Applications</Headline>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      {categories &&
+        categories.map(({ name, items }, idx) => (
+          <AppCategory key={[name, idx].join("")} name={name} items={items} />
+        ))}
+      {apps && (
+        <AppCategory
+          name={categories.length > 0 ? "Uncategorized apps" : ""}
+          items={apps}
+        />
+      )}
+    </ListContainer>
+  );
+};
+
+export default AppList;
---
+++ src/components/icon.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import selectedTheme from "./themeManager";
+
+const Icon = styled.i`
+  font-family: "Material Icons";
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  text-color: ${selectedTheme.mainColor};
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: "liga";
+`;
+
+export default Icon;
