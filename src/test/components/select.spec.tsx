30657f04d3422a7d08ff71942345146fd75d7205
Bastian Meissner <meissner.bastian@gmail.com>
2021-11-26 18:15:39 +0100

    Improve test coverage for select.tsx

--- src/test/components/select.spec.tsx
+++ src/test/components/select.spec.tsx
@@ -14,7 +14,7 @@ const items: Array<IItemProps> = [
 ];
 
 describe("select.tsx", () => {
-  it("Tests select rendering", () => {
+  it("Tests Select rendering", () => {
     const { asFragment } = render(
       <Select items={items} onChange={(item) => onChange(item)}></Select>,
     );
@@ -22,7 +22,26 @@ describe("select.tsx", () => {
     expect(asFragment).toMatchSnapshot();
   });
 
-  it("Tests select onChange", () => {
+  it("Tests rendering of multiple Select elements", () => {
+    const { asFragment } = render(
+      <>
+        <Select
+          testId="1"
+          items={items}
+          onChange={(item) => onChange(item)}
+        ></Select>
+        <Select
+          testId="2"
+          items={items}
+          onChange={(item) => onChange(item)}
+        ></Select>
+      </>,
+    );
+
+    expect(asFragment).toMatchSnapshot();
+  });
+
+  it("Tests Select onChange", () => {
     const select = render(
       <Select items={items} onChange={(item) => onChange(item)}></Select>,
     );
@@ -31,12 +50,37 @@ describe("select.tsx", () => {
     expect(onChange).toBeCalledWith(items[1]);
   });
 
-  it("Tests select onChange with undefined item", () => {
+  it("Tests Select onChange with undefined item", () => {
     const select = render(
       <Select items={items} onChange={(item) => onChange(item)}></Select>,
     );
 
     fireEvent.change(select.getByTestId("select"), { target: { value: 5 } });
     expect(onChange).toBeCalledWith(undefined);
+  });
+
+  it("Tests `current`-value without testId", () => {
+    const { asFragment } = render(
+      <Select
+        current={items[1].label}
+        items={items}
+        onChange={(item) => onChange(item)}
+      ></Select>,
+    );
+
+    expect(asFragment).toMatchSnapshot();
+  });
+
+  it("Tests `current`-value with testId", () => {
+    const { asFragment } = render(
+      <Select
+        current={items[1].label}
+        testId="1"
+        items={items}
+        onChange={(item) => onChange(item)}
+      ></Select>,
+    );
+
+    expect(asFragment).toMatchSnapshot();
   });
 });
