9c76a431444683119b0b8b9b7bc58e10ad6fc17a
Bastian Meissner <meissner.bastian@gmail.com>
2021-11-26 21:16:10 +0100

    Update formatting of useMediaQuery.tsx

--- src/lib/useMediaQuery.tsx
+++ src/lib/useMediaQuery.tsx
@@ -6,12 +6,9 @@ const useMediaQuery = (query: string) => {
 
   useEffect(() => {
     const media = window.matchMedia(query);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
-    }
-    const listener = () => {
-      setMatches(media.matches);
-    };
+    if (media.matches !== matches) setMatches(media.matches);
+    const listener = () => setMatches(media.matches);
+
     media.addEventListener("change", listener);
     return () => media.removeEventListener("change", listener);
   }, [matches, query]);
